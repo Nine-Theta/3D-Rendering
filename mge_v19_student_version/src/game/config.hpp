d1119992833f26193d8ca5132e2b13b842b88bc0
David <davidheetebrij@gmail.com>
2019-12-09 14:39:54 +0100

    WeekOne_Init

---
+++ mge_v19_student_version/src/game/config.hpp
@@ -0,0 +1,7 @@
+#include <string>
+namespace config {
+    const std::string MGE_MODEL_PATH ("game/models/");
+    const std::string MGE_TEXTURE_PATH ("game/textures/");
+    const std::string MGE_SHADER_PATH ("game/shaders/");
+    const std::string MGE_FONT_PATH ("game/fonts/");
+}
