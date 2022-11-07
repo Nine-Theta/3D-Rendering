529f4f3eeca666d839868bc381167555799870f6
Nine <davidheetebrij@gmail.com>
2022-11-07 15:42:44 +0100

    Initial Setup

---
+++ mge_v19_student_version/src/RenderingProject/config.hpp
@@ -0,0 +1,7 @@
+#include <string>
+namespace config {
+    const std::string MGE_MODEL_PATH ("RenderingProject/models/");
+    const std::string MGE_TEXTURE_PATH ("RenderingProject/textures/");
+    const std::string MGE_SHADER_PATH ("RenderingProject/shaders/");
+    const std::string MGE_FONT_PATH ("RenderingProject/fonts/");
+}
